f29a79049ef7cdba4033a8f557b7bb26791a4528
Mukesh Soni <mukeshsoni@gmail.com>
2016-06-11 19:56:40 +0530

    added intersection function

---
+++ test/array/intersection_test.js
@@ -0,0 +1,14 @@
+import intersection from '../../src/array/intersection'
+import chai from 'chai'
+let expect = chai.expect
+
+describe('intersection', function () {
+    it('should intersection arr values as per the function provided', function () {
+        expect(intersection([2, 1], [2, 3])).to.eql([2])
+        expect(intersection([2, 1, 5], [2, 5, 3])).to.eql([2, 5])
+        expect(intersection([2, 1], [4, 3])).to.eql([])
+        expect(intersection([2, 1], [])).to.eql([])
+        expect(intersection([], [])).to.eql([])
+        expect(intersection([], [2, 4])).to.eql([])
+    });
+});
