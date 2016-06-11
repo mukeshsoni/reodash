be73f2395537a0535a61db26fdfbf5552f78f6d7
Mukesh Soni <mukeshsoni@gmail.com>
2016-06-11 13:40:00 +0530

    Removed tests folder

--- tests/reduce_test.js
+++
@@ -1,9 +0,0 @@
-import reduce from '../../src/array/reduce'
-import chai from 'chai'
-let expect = chai.expect
-
-describe('reduce', function () {
-    it('should add number', function () {
-        expect(reduce([1,2,4], (a, b) => a + b)).to.equal(7)
-    });
-});
